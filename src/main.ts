73f2499bf3e1b21643b4670c1868d152e57db966
Pedro Lages <pedro.slages3@gmail.com>
2023-03-28 17:00:45 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 //teste
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://www.dynoprojects.com.br',
+      'https://www.dynoprojects.com.br',
+      'http://localhost:3001',
+    ],
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
