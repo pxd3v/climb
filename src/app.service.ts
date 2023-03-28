0c7486e28759d2e3bd6a191b476d905595483d5d
Pedro Lages <pedro.slages3@gmail.com>
2023-03-28 17:25:58 -0300

    feat(health-check): improve

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  checkHealth(): string {
+    const isWrongDb =
+      process.env.NODE_ENV === 'production' &&
+      process.env.DATABASE_URL.includes('localhost');
+    if (isWrongDb) {
+      throw new HttpException('DANGER!', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    return 'Healthy!';
   }
 }
