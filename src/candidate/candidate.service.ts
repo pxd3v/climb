aa54317cfe9d0324d3526d72df4f1c236150ed16
Pedro Lages <pedro.slages3@gmail.com>
2023-04-09 18:40:53 -0300

    feat(candidate): get candidate user data from candidate number and event id

--- src/candidate/candidate.service.ts
+++ src/candidate/candidate.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { Candidate, Prisma } from '@prisma/client';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Candidate, Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class CandidateService {
-  constructor(private prisma: PrismaService) {
+  constructor(private prisma: PrismaService, private userService: UserService) {
     // prisma.$on<any>('query', (event: Prisma.QueryEvent) => {
     //   console.log('Query: ' + event.query);
     //   console.log('Duration: ' + event.duration + 'ms');
@@ -17,5 +18,28 @@ export class CandidateService {
     return this.prisma.candidate.findUnique({
       where: candidateWhereUniqueInput,
     });
+  }
+
+  async getCandidateUserData(
+    candidateWhere: Prisma.CandidateWhereUniqueInput,
+  ): Promise<User | null> {
+    const candidate = await this.prisma.candidate.findUnique({
+      where: candidateWhere,
+    });
+
+    if (!candidate) {
+      console.log('@@error', {
+        err: 'Candidato não encontrado.',
+        params: {
+          ...candidateWhere,
+        },
+      });
+      throw new HttpException(
+        'Candidato não encontrado.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return this.userService.user({ id: candidate.userId });
   }
 }
