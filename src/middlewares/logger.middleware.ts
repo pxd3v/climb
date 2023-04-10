acc5eb1afcfbe7ce7d6562c70ca5ab28c72b3b0e
Pedro Lages <pedro.slages3@gmail.com>
2023-04-09 21:27:06 -0300

    feat(logger): improve request loggers and add response logger (#2)

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,13 +1,37 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('[Request]:', {
-      params: req.params,
+    const { password, ...body } = req.body; // don't log password
+    const { method, originalUrl } = req;
+    const params = JSON.stringify({
       query: req.query,
-      body: req.body,
+      body: body,
+    });
+    const { authorization, ...filteredHeaders } = req.headers; // don't log authorization
+    const headers = JSON.stringify(filteredHeaders);
+
+    this.logger.log(`
+      [Request]: 
+        method: ${method} 
+        url: ${originalUrl} 
+        params: ${params}
+        headers: ${headers}
+    `);
+
+    res.on('finish', () => {
+      const user = JSON.stringify(req.user);
+      this.logger.log(`
+      [Response]: 
+        method: ${method} 
+        url: ${originalUrl} 
+        user: ${user}
+        code: ${res.statusCode}
+        params: ${params}
+      `);
     });
     next();
   }
